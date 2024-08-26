f40ee5a9e2569325e76c89aff16f25658d5bd47a
JuwoenLee <82970714+JuwoenLee@users.noreply.github.com>
2024-08-26 12:51:38 +0900

    [Silver IV] Title: 카드2, Time: 180 ms, Memory: 50968 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,12 +1,15 @@
-import sys
+# 카드 뭉치에서 제일 위의 카드를 버리고 그 다음 카드를 맨 뒤로 옮겨서
+# 마지막 1장이 남을 때까지 반복, 그 카드를 출력하는 문제
+
 from collections import deque
+import sys
 input = sys.stdin.readline
 
-N = int(input())
-Q = deque(range(1, N + 1))
+n = int(input())
+queue = deque(range(1, n + 1))
 
-while(len(Q) > 1) : 
-    Q.popleft()
-    Q.append(Q.popleft())
+while len(queue) != 1 :
+    queue.popleft()
+    queue.append(queue.popleft())
 
-print(Q.popleft())
+print(queue.popleft())
