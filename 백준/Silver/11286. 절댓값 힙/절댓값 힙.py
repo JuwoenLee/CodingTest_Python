eff415ffd39debc0192896c929560d7db1884ed6
JuwoenLee <82970714+JuwoenLee@users.noreply.github.com>
2024-09-03 11:57:21 +0900

    [Silver I] Title: 절댓값 힙, Time: 256 ms, Memory: 45004 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,17 +1,29 @@
-import sys
+# 우선순위 큐
+
 from queue import PriorityQueue
+import sys
 input = sys.stdin.readline
 
-N = int(input())
-pQ = PriorityQueue(maxsize=N)
-ans = []
+n = int(input())
+pq = PriorityQueue(maxsize=n)
+ans = list()
 
-for i in range(N):
+for i in range(n) :
     x = int(input())
-    if x != 0 : pQ.put((abs(x), x))
-    else :
-        if pQ.empty() : ans.append(0)
-        else : ans.append(pQ.get()[1])
 
+    # 입력이 0이 안니라면 (추가)
+    if x != 0 :
+        # 절댓값으로 우선순위와 원래 값 저장
+        pq.put((abs(x), x))
+    # 입력이 0이라면
+    else : 
+        # pq가 비어있다면 (0 출력)
+        if pq.empty() :
+            ans.append(0)
+        # pq가 비어있지 않다면 (절댓값이 가장 작은 값)
+        else : 
+            ans.append(pq.get()[1])
+
+# 답 출력
 for a in ans :
     print(a)
