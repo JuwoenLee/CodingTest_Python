5cd868f22c25d75236281a117a52e170d4758d99
JuwoenLee <82970714+JuwoenLee@users.noreply.github.com>
2024-09-21 21:34:47 +0900

    [Silver III] Title: 파도반 수열, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+arr = [0, 1, 1, 1]
+
+for i in range(4, 101) :
+    arr.append(arr[i - 2] + arr[i - 3])
+
+for _ in range(t) :
+    print(arr[int(input())])
